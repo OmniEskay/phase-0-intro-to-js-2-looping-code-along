7026fbc867192692a80172e62d9fba3e6293a064
+ <omnieskay@proton.me>
2025-03-29 17:04:44 +0300

    add for loop in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray) {
+    const messages = namesArray.map(name => {
+      return `Thank you, ${name}, for the wonderful surprise gift!`;
+    });
+    return messages;
+  }
+
+  function countDown(startNumber) {
+    for (let i = startNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+  
